f088ddb71baf45764c8724776086e78c812c588b
Mary1812 <mariyapiligrim3@gmail.com>
2021-10-07 17:07:42 +0300

    Add useCallback

--- src/components/HelloUserCard/index.jsx
+++ src/components/HelloUserCard/index.jsx
@@ -1,13 +1,26 @@
 import { UserContext } from "../../contexts";
-import {useContext} from "react"
-
+import { useCallback, useContext, useEffect, useState } from "react";
 
 function HelloUserCard() {
-  const [user] = useContext(UserContext)
-  return (<div style={{ display: 'flex', justifyContent: 'flex-end' }}>
-    <h1>Hello {user.fullName}</h1>
-    <img src={user.userImg} alt="img" style={{ width: '100px' }}></img>
-  </div>)
+  const [user] = useContext(UserContext);
+  const [value, setValue] = useState("hello user");
+
+  const alertValue = useCallback(() => {
+    console.log(value);
+  }, [value]);
+
+  useEffect(() => {
+    console.log("alert есть");
+  }, [alertValue]);
+
+
+  return (
+    <div style={{ display: "flex", justifyContent: "flex-end" }}>
+      <h1>Hello {user.fullName}</h1>
+      <img src={user.userImg} alt="img" style={{ width: "100px" }}></img>
+      <button onClick={alertValue}>ALERT</button>
+    </div>
+  );
 }
 
 export default HelloUserCard;
